4900e7ba1193b5386499459b725deff809d3bcb5
bhanly <bhanly@mit.edu>
2024-12-08 20:20:20 -0500

    attempting dsp top level stuff

--- test_code/DSP_RTL_Verif/sim/sim_build/cocotb_iverilog_dump.v
+++ test_code/DSP_RTL_Verif/sim/sim_build/cocotb_iverilog_dump.v
@@ -1,6 +1,6 @@
 module cocotb_iverilog_dump();
 initial begin
-    $dumpfile("/home/alyssa/6.s965/final_project/sim/sim_build/compute_coeff_averaging.fst");
-    $dumpvars(0, compute_coeff_averaging);
+    $dumpfile("/Users/biancahanly/ADL5960_RFSoC_VNA/test_code/DSP_RTL_Verif/sim/sim_build/DSP_top_level.fst");
+    $dumpvars(0, DSP_top_level);
 end
 endmodule
