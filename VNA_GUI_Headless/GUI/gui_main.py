a9b2de0cabd5bf12afca07aebf964cb5f7e9a52d
tklouie <ltiffany2002@gmail.com>
2024-12-08 19:27:16 -0500

    updating gui for parameters

--- VNA_GUI_Headless/GUI/gui_main.py
+++ VNA_GUI_Headless/GUI/gui_main.py
@@ -4,6 +4,8 @@ import sys
 import asyncio
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QRadioButton, QHBoxLayout, QVBoxLayout
+from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
 import pyqtgraph as pg
 import numpy as np
 from networking import RemoteConnection
@@ -18,24 +20,62 @@ class MainWindow(QWidget):
         self.layout = QtWidgets.QGridLayout()
         self.setLayout(self.layout)
 
+        '''
+        Header Layout
+        '''
+        self.title_label = QLabel("SusVNA Interface")
+        title_font = QFont()
+        title_font.setPointSize(20)
+        title_font.setBold(True)
+        self.title_label.setFont(title_font)
+        self.title_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.subtitle_label = QLabel("Copyright 2024 Cruftsight Technologies")
+        layout_font = QFont()
+        layout_font.setPointSize(10)
+        layout_font.setBold(False)
+        self.subtitle_label.setAlignment(Qt.AlignmentFlag.AlignHCenter)
+
+        self.title_layout = QtWidgets.QGridLayout()
+        self.title_layout.addWidget(self.title_label)
+        self.title_layout.addWidget(self.subtitle_label)
+
+        '''
+        Plot Layout
+        '''
         self.plot_adc_widget = pg.PlotWidget()
         self.plot_adc_widget.setXRange(0, 500)
         self.plot_adc_widget.setYRange(-2000, 2000)
-
+        self.plot_adc_widget.setLabel("left", "ADC Value")
+        self.plot_adc_widget.setLabel("bottom", "Time")
+        self.plot_adc_widget.setTitle("Live ADC Values", size="10pt")
+        
         self.plot_s_param_widget = pg.PlotWidget()
         #self.plot_s_param_widget.setXRange(0, 500) #Need to set frequency range
         self.plot_s_param_widget.setYRange(-200, 10) #Setting top range
+        self.plot_s_param_widget.setLabel("left", "S Parameter")
+        self.plot_s_param_widget.setLabel("bottom", "Freq (Hz)")
+        self.plot_s_param_widget.setTitle("Live S-Parameters", size="10pt")
 
         self.filtered_port1_forward_plot = self.plot_adc_widget.plot(pen = 'r')
         self.filtered_port2_forward_plot = self.plot_adc_widget.plot(pen = 'g')
         self.filtered_port1_reverse_plot = self.plot_adc_widget.plot(pen = 'b')
         self.filtered_port2_reverse_plot = self.plot_adc_widget.plot(pen = 'w')
+        self.plot_adc_widget.addLegend()
 
         self.s11_plot = self.plot_s_param_widget.plot(pen = 'r')
         self.s12_plot = self.plot_s_param_widget.plot(pen = 'g')
         self.s21_plot = self.plot_s_param_widget.plot(pen = 'b')
         self.s22_plot = self.plot_s_param_widget.plot(pen = 'w')
+        self.plot_s_param_widget.addLegend()
 
+        self.plot_layout = QtWidgets.QGridLayout()
+        self.plot_layout.addWidget(self.plot_adc_widget, 0, 0)
+        self.plot_layout.addWidget(self.plot_s_param_widget, 0, 1)
+
+        '''
+        Source Controls
+        '''
         self.mode_label = QLabel("Sweep mode:")
         self.mode_radio_button_zerospan = QtWidgets.QRadioButton("Zero Span")
         self.mode_radio_button_startend = QtWidgets.QRadioButton("Start/End")
@@ -44,6 +84,15 @@ class MainWindow(QWidget):
         self.mode_radio_button_startend.toggled.connect(self.mode_startend)
         self.mode_radio_button_centerspan.toggled.connect(self.mode_centerspan)
 
+        self.controls_layout = QtWidgets.QGridLayout()
+        self.controls_layout.addWidget(self.mode_label, 0, 0)
+        self.controls_layout.addWidget(self.mode_radio_button_zerospan, 0, 1)
+        self.controls_layout.addWidget(self.mode_radio_button_startend, 0, 2)
+        self.controls_layout.addWidget(self.mode_radio_button_centerspan, 0, 3)
+    
+        '''
+        Cal Controls Layout
+        '''
         self.short_push_button = QtWidgets.QPushButton("Short Calibrate")
         self.open_push_button = QtWidgets.QPushButton("Open Calibrate")
         self.load_push_button = QtWidgets.QPushButton("Load Calibrate")
@@ -56,27 +105,65 @@ class MainWindow(QWidget):
         self.cal_layout.addWidget(self.open_push_button, 1, 0)
         self.cal_layout.addWidget(self.load_push_button, 2, 0)
 
+        '''
+        Update Button Layout
+        '''
         update_button = QtWidgets.QPushButton("Update Output", self)
         update_button.setGeometry(200, 150, 100, 100)
         update_button.clicked.connect(self.update_call)
-
-        self.controls_layout = QtWidgets.QGridLayout()
-        self.layout.addWidget(self.plot_adc_widget, 0, 0)
-        self.layout.addWidget(self.plot_s_param_widget, 0, 1)
-        self.layout.addWidget(update_button, 0, 2)
-        self.layout.addLayout(self.cal_layout, 1, 2)
         
-        self.layout.addLayout(self.controls_layout, 1, 0)
-        self.controls_layout.addWidget(self.mode_label, 0, 0)
-        self.controls_layout.addWidget(self.mode_radio_button_zerospan, 0, 1)
-        self.controls_layout.addWidget(self.mode_radio_button_startend, 0, 2)
-        self.controls_layout.addWidget(self.mode_radio_button_centerspan, 0, 3)
-    
+        '''
+        Final Layout
+        '''
+        self.layout.addLayout(self.title_layout, 0, 0)
+        self.layout.addLayout(self.plot_layout, 1, 0)
+        self.layout.addWidget(update_button, 1, 2)
+        self.layout.addLayout(self.cal_layout, 2, 2)
+        self.layout.addLayout(self.controls_layout, 2, 0)
+
         self.show()
 
     def update_call(self):
-        print("pressed")
-        #Fill in any call functions to update here
+        if self.mode_radio_button_zerospan.isChecked():
+            parameter_list = {
+                "type" : "parameters",
+                "single_freq": self.zero_span_frequency_input.text(),
+                "start_freq": 0,
+                "stop_freq": 0,
+                "center_freq": 0,
+                "span": 0,
+                "resolution": 0
+            }
+        elif self.mode_radio_button_startend.isChecked():
+            parameter_list = {
+                "type" : "parameters",
+                "single_freq": 0,
+                "start_freq": self.start_frequency_input.text(),
+                "stop_freq": self.stop_frequency_input.text(),
+                "center_freq": 0,
+                "span": 0,
+                "resolution": self.pointspersweep_input.text()
+            }
+        elif self.mode_radio_button_centerspan.isChecked():
+            parameter_list = {
+                "type" : "parameters",
+                "start_freq": 0,
+                "stop_freq": 0,
+                "center_freq": self.center_frequency_input.text(),
+                "span": self.span_frequency_input.text(),
+                "resolution": self.pointspersweep_input.text()
+            }
+        else:
+            parameter_list = {
+                "type" : "parameters",
+                "start_freq": 0,
+                "stop_freq": 0,
+                "center_freq": 0,
+                "span": 0,
+                "resolution": 0
+            }
+        
+        remote_connection.send_parameters(parameter_list)
     
     def clear_controls(self):
         """Clear all widgets and layouts from the second row of the controls layout."""
@@ -106,8 +193,8 @@ class MainWindow(QWidget):
             self.zero_span_frequency_label = QtWidgets.QLabel("Frequency:")
             self.zero_span_frequency_label_mhz = QtWidgets.QLabel("MHz")
             self.zero_span_frequency_input = QtWidgets.QLineEdit()
-            self.zero_span_layout = QtWidgets.QGridLayout()
 
+            self.zero_span_layout = QtWidgets.QGridLayout()
             self.zero_span_layout.addWidget(self.zero_span_frequency_label, 0, 0)
             self.zero_span_layout.addWidget(self.zero_span_frequency_input, 0, 1)
             self.zero_span_layout.addWidget(self.zero_span_frequency_label_mhz, 0, 2)
@@ -153,26 +240,26 @@ class MainWindow(QWidget):
             print("Center/span mode")
             self.clear_controls()
 
-            center_frequency_label = QtWidgets.QLabel("Center Freq:")
-            center_frequency_label_mhz = QtWidgets.QLabel("MHz")
-            center_frequency_input = QtWidgets.QLineEdit()
+            self.center_frequency_label = QtWidgets.QLabel("Center Freq:")
+            self.center_frequency_label_mhz = QtWidgets.QLabel("MHz")
+            self.center_frequency_input = QtWidgets.QLineEdit()
 
-            span_frequency_label = QtWidgets.QLabel("Span:")
-            span_frequency_label_mhz = QtWidgets.QLabel("MHz")
-            span_frequency_input = QtWidgets.QLineEdit()
+            self.span_frequency_label = QtWidgets.QLabel("Span:")
+            self.span_frequency_label_mhz = QtWidgets.QLabel("MHz")
+            self.span_frequency_input = QtWidgets.QLineEdit()
 
             self.pointspersweep_label = QtWidgets.QLabel("Points Per Sweep:")
             self.pointspersweep_input = QtWidgets.QLineEdit()
 
             self.center_freq_layout = QtWidgets.QGridLayout()
-            self.center_freq_layout.addWidget(center_frequency_label, 1, 0)
-            self.center_freq_layout.addWidget(center_frequency_input, 1, 1)
-            self.center_freq_layout.addWidget(center_frequency_label_mhz, 1, 2)
+            self.center_freq_layout.addWidget(self.center_frequency_label, 1, 0)
+            self.center_freq_layout.addWidget(self.center_frequency_input, 1, 1)
+            self.center_freq_layout.addWidget(self.center_frequency_label_mhz, 1, 2)
 
             self.span_layout = QtWidgets.QGridLayout()
-            self.span_layout.addWidget(span_frequency_label, 2, 0)
-            self.span_layout.addWidget(span_frequency_input, 2, 1)
-            self.span_layout.addWidget(span_frequency_label_mhz, 2, 2)
+            self.span_layout.addWidget(self.span_frequency_label, 2, 0)
+            self.span_layout.addWidget(self.span_frequency_input, 2, 1)
+            self.span_layout.addWidget(self.span_frequency_label_mhz, 2, 2)
             
             self.pointspersweep_layout = QtWidgets.QGridLayout()
             self.pointspersweep_layout.addWidget(self.pointspersweep_label, 0, 0)
@@ -184,12 +271,33 @@ class MainWindow(QWidget):
 
     def cal_short(self):
         print("Short calibration triggered.")
+        parameter_list = {
+                "type" : "calibration",
+                "short": 1,
+                "open": 0,
+                "load": 0
+            }
+        remote_connection.send_parameters(parameter_list)
 
     def cal_open(self):
         print("Open calibration triggered.")
+        parameter_list = {
+                "type" : "calibration",
+                "short": 0,
+                "open": 1,
+                "load": 0
+            }
+        remote_connection.send_parameters(parameter_list)
 
     def cal_load(self):
         print("Load calibration triggered.")
+        parameter_list = {
+                "type" : "calibration",
+                "short": 0,
+                "open": 0,
+                "load": 1
+            }
+        remote_connection.send_parameters(parameter_list)
 
 
 if __name__ == "__main__":
