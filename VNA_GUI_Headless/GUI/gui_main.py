Fixed bugs in S-param calculation. Getting pretty good params.

VNA_GUI_Headless/GUI/gui_main.py
@@ -9,6 +9,7 @@ from PyQt6.QtCore import Qt
 import pyqtgraph as pg
 import numpy as np
 from networking import RemoteConnection
+import skrf as rf
 
 remote_connection = RemoteConnection("0.0.0.0", 1234)
 
@@ -45,29 +46,28 @@ class MainWindow(QWidget):
         '''
         self.plot_adc_widget = pg.PlotWidget()
         self.plot_adc_widget.setXRange(0, 500)
-        self.plot_adc_widget.setYRange(-2000, 2000)
+        self.plot_adc_widget.setYRange(-8000, 8000)
         self.plot_adc_widget.setLabel("left", "ADC Value")
         self.plot_adc_widget.setLabel("bottom", "Time")
         self.plot_adc_widget.setTitle("Live ADC Values", size="10pt")
         
         self.plot_s_param_widget = pg.PlotWidget()
-        #self.plot_s_param_widget.setXRange(0, 500) #Need to set frequency range
-        self.plot_s_param_widget.setYRange(-200, 10) #Setting top range
+        self.plot_s_param_widget.setYRange(-80, 20) #Setting top range
         self.plot_s_param_widget.setLabel("left", "S Parameter")
-        self.plot_s_param_widget.setLabel("bottom", "Freq (Hz)")
+        self.plot_s_param_widget.setLabel("bottom", "Freq (MHz)")
         self.plot_s_param_widget.setTitle("Live S-Parameters", size="10pt")
 
-        self.filtered_port1_forward_plot = self.plot_adc_widget.plot(pen = 'r')
-        self.filtered_port2_forward_plot = self.plot_adc_widget.plot(pen = 'g')
-        self.filtered_port1_reverse_plot = self.plot_adc_widget.plot(pen = 'b')
-        self.filtered_port2_reverse_plot = self.plot_adc_widget.plot(pen = 'w')
         self.plot_adc_widget.addLegend()
+        self.filtered_port1_forward_plot = self.plot_adc_widget.plot(pen = 'r', name = "Port 1 Forward")
+        self.filtered_port2_forward_plot = self.plot_adc_widget.plot(pen = 'g', name = "Port 2 Forward")
+        self.filtered_port1_reverse_plot = self.plot_adc_widget.plot(pen = 'b', name = "Port 1 Reverse")
+        self.filtered_port2_reverse_plot = self.plot_adc_widget.plot(pen = 'w', name = "Port 2 Reverse")
 
-        self.s11_plot = self.plot_s_param_widget.plot(pen = 'r')
-        self.s12_plot = self.plot_s_param_widget.plot(pen = 'g')
-        self.s21_plot = self.plot_s_param_widget.plot(pen = 'b')
-        self.s22_plot = self.plot_s_param_widget.plot(pen = 'w')
         self.plot_s_param_widget.addLegend()
+        self.s11_plot = self.plot_s_param_widget.plot(pen = 'r', name = "S11")
+        self.s12_plot = self.plot_s_param_widget.plot(pen = 'g', name = "S12")
+        self.s21_plot = self.plot_s_param_widget.plot(pen = 'b', name = "S21")
+        self.s22_plot = self.plot_s_param_widget.plot(pen = 'w', name = "S22")
 
         self.plot_layout = QtWidgets.QGridLayout()
         self.plot_layout.addWidget(self.plot_adc_widget, 0, 0)
@@ -299,6 +299,12 @@ class MainWindow(QWidget):
             }
         remote_connection.send_parameters(parameter_list)
 
+def mag_db(s_param):
+    values = s_param.s[:,0,0]
+    mag_db = np.zeros(values.shape, dtype = np.float64)
+    mag_db[values == 0] = float("Inf")
+    mag_db[values != 0] = 20 * np.log10(np.abs(values[values != 0]))
+    return mag_db
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -311,6 +317,11 @@ if __name__ == "__main__":
             window.filtered_port2_forward_plot.setData(remote_connection.data["filtered_port2_forward"])
             window.filtered_port1_reverse_plot.setData(remote_connection.data["filtered_port1_reverse"])
             window.filtered_port2_reverse_plot.setData(remote_connection.data["filtered_port2_reverse"])
-
+            raw_s_parameters = remote_connection.data["raw_s_parameters"]
+            window.plot_s_param_widget.setXRange(raw_s_parameters.f[0], raw_s_parameters.f[-1])
+            window.s11_plot.setData(raw_s_parameters.f, mag_db(raw_s_parameters.s11))
+            window.s12_plot.setData(raw_s_parameters.f, mag_db(raw_s_parameters.s12))
+            window.s21_plot.setData(raw_s_parameters.f, mag_db(raw_s_parameters.s21))
+            window.s22_plot.setData(raw_s_parameters.f, mag_db(raw_s_parameters.s22))
 
         app.processEvents()

VNA_GUI_Headless/Headless/data_process.py
@@ -9,6 +9,9 @@ import math
 import numpy as np
 from scipy import signal
 
+def polar_to_rectangular(magnitude, angle):
+    return magnitude * np.exp(1j * angle)
+
 class DSP:
     def __init__(self, if_bandwidth = 10e3):
         self.fir_filter = signal.firwin(15, [1e6 - if_bandwidth / 2, 1e6 + if_bandwidth / 2], fs = 147.456e6, pass_zero = False)
@@ -38,14 +41,12 @@ class DSP:
         reverse_phase = np.angle(reverse_signal)
 
         # Reflection coefficient magnitude comes from the ratios of the magnitudes of the forward and reverse signals.
-        gamma_magnitude = np.mean(forward_magnitude) / np.mean(reverse_magnitude)
+        gamma_magnitude = np.mean(reverse_magnitude) / np.mean(forward_magnitude)
 
         # Reflection coefficient phase comes from the difference in phase of the forward and reverse signals. 
         # Unwrapping performed by subtracting 2pi when we are outside the 180 deg range. Not 100% sure this works under all conditions.
-        phase_difference = forward_phase - reverse_phase
+        phase_difference = reverse_phase - forward_phase
         phase_difference[phase_difference > math.pi] = phase_difference[phase_difference > math.pi] - 2 * math.pi
         gamma_phase = np.mean(phase_difference)
-        
-        gamma_magnitude_db = 20*math.log(gamma_magnitude, 10)
 
-        return gamma_magnitude_db, gamma_phase
+        return gamma_magnitude, gamma_phase

VNA_GUI_Headless/Headless/headless_main.py
@@ -5,10 +5,11 @@ import time
 import math
 import numpy as np
 import zlib
+import skrf as rf
 
 from networking import GuiRemoteConnection
 from sig_source import SigSource
-from data_process import DSP
+from data_process import DSP, polar_to_rectangular
 from adl5960 import ADL5960
 from switch import Switch
 from adcs import ADCs
@@ -37,8 +38,8 @@ adcs = ADCs(ol.usp_rf_data_converter_0, ol.axi_dma_0)
 
 # Set up SPI devices
 print("Setting up VNA frontends...")
-adl1 = ADL5960(ol.spi_adl_0)
-adl2 = ADL5960(ol.spi_adl_1)
+adl1 = ADL5960(ol.spi_adl_0, fgain = 30, rgain = 36)
+adl2 = ADL5960(ol.spi_adl_1, fgain = 30, rgain = 36)
 print("Setting up source...")
 source = SigSource(mmio_spi_controller = ol.spi_lmx_0)
 source.set_active()
@@ -48,8 +49,11 @@ dsp = DSP()
 
 #Main capture and processing loop.
 #filename = "siggen"
-data_out_file = open("bandpass.csv", "w")
-data_out_file.write(f"Freq (Hz),S11_mag,S11_phase,S12_mag,S12_phase,S21_mag,S21_phase,S22_mag,S22_phase\n")
+#data_out_file = open("bandpass.csv", "w")
+#data_out_file.write(f"Freq (Hz),S11_mag,S11_phase,S12_mag,S12_phase,S21_mag,S21_phase,S22_mag,S22_phase\n")
+
+raw_s_parameters = rf.Network(s = np.zeros((len(source.freq_points), 2, 2), dtype = np.float64), f = source.freq_points)
+
 while True:
     freq = source.get_current_freq()
 
@@ -74,7 +78,16 @@ while True:
 
     # Calculate S-parameters S11 and S12
     S11_mag, S11_phase = dsp.calculate_S_param(filtered_port1_forward, filtered_port1_reverse)
-    S12_mag, S12_phase = dsp.calculate_S_param(filtered_port1_forward, filtered_port2_forward)
+    S12_mag, S12_phase = dsp.calculate_S_param(filtered_port2_forward, filtered_port1_forward)
+
+    # Send data to network GUI.
+    message = {}
+    message["filtered_port1_forward"] = filtered_port1_forward[:500].real.tobytes()
+    message["filtered_port2_forward"] = filtered_port2_forward[:500].real.tobytes()
+    message["filtered_port1_reverse"] = filtered_port1_reverse[:500].real.tobytes()
+    message["filtered_port2_reverse"] = filtered_port2_reverse[:500].real.tobytes()
+    message["raw_s_parameters"] = raw_s_parameters
+    remote_connection.send_message(message)
 
     # Set switch
     switch.setPort2Active()
@@ -99,13 +112,22 @@ while True:
     S21_mag, S21_phase = dsp.calculate_S_param(filtered_port2_forward, filtered_port1_forward)
     S22_mag, S22_phase = dsp.calculate_S_param(filtered_port2_forward, filtered_port2_reverse)
 
+    # Save raw S-parameters.
+    raw_s_parameters.s[source.get_current_index(), 0, 0] = polar_to_rectangular(S11_mag, S11_phase)
+    raw_s_parameters.s[source.get_current_index(), 0, 1] = polar_to_rectangular(S12_mag, S12_phase)
+    raw_s_parameters.s[source.get_current_index(), 1, 0] = polar_to_rectangular(S21_mag, S21_phase)
+    raw_s_parameters.s[source.get_current_index(), 1, 1] = polar_to_rectangular(S22_mag, S22_phase)
+
     # Send data to network GUI.
+    """
     message = {}
     message["filtered_port1_forward"] = filtered_port1_forward[:500].real.tobytes()
     message["filtered_port2_forward"] = filtered_port2_forward[:500].real.tobytes()
     message["filtered_port1_reverse"] = filtered_port1_reverse[:500].real.tobytes()
     message["filtered_port2_reverse"] = filtered_port2_reverse[:500].real.tobytes()
+    message["raw_s_parameters"] = raw_s_parameters
     remote_connection.send_message(message)
+    """
 
     # Check for messages.
     message = remote_connection.receive_message()
@@ -114,7 +136,9 @@ while True:
         if message["type"] == "parameters":
             source.update_parameters(start = message["start"], stop = message["stop"], center = message["center"], span = message["span"], resolution = message["resolution"], single_freq = message["single_freq"])
             source.set_next_freq(start = True)
+            raw_s_parameters = rf.Network(s = np.zeros((len(source.freq_points), 2, 2), dtype = np.complex128), f = source.freq_points)
 
+    """
     print(f"Frequency: {freq} MHz")
     print(f"Calculated S11 magnitude is {S11_mag:.2f}.")
     print(f"Calculated S11 phase is {S11_phase * 180 / math.pi:.2f}\N{DEGREE SIGN}.")
@@ -124,11 +148,12 @@ while True:
     print(f"Calculated S21 phase is {S21_phase * 180 / math.pi:.2f}\N{DEGREE SIGN}.")
     print(f"Calculated S22 magnitude is {S22_mag:.2f}.")
     print(f"Calculated S22 phase is {S22_phase * 180 / math.pi:.2f}\N{DEGREE SIGN}.")
+    """
 
     source.set_next_freq()
 
     #np.savez(f"{filename}_s_parameters.npz", S11_mag = S11_mag, S11_phase = S11_phase, S12_mag = S12_mag, S12_phase = S12_phase, S21_mag = S21_mag, S21_phase = S21_phase, S22_mag = S22_mag, S22_phase = S22_phase)
 
     #break
-    data_out_file.write(f"{freq},{S11_mag},{S11_phase},{S12_mag},{S12_phase},{S21_mag},{S21_phase},{S22_mag},{S22_phase}\n")
-    data_out_file.flush()
+    #data_out_file.write(f"{freq},{S11_mag},{S11_phase},{S12_mag},{S12_phase},{S21_mag},{S21_phase},{S22_mag},{S22_phase}\n")
+    #data_out_file.flush()

test_code/dsp_reference_implementation/dsp_test.py
@@ -49,10 +49,10 @@ reverse_magnitude = np.abs(reverse_filtered)
 reverse_phase = np.angle(reverse_filtered)
 
 # Reflection coefficient magnitude comes from the ratios of the magnitudes of the forward and reverse signals.
-gamma_magnitude = np.mean(forward_magnitude) / np.mean(reverse_magnitude)
+gamma_magnitude = np.mean(reverse_magnitude) / np.mean(forward_magnitude)
 
 # Reflection coefficient phase comes from the difference in phase of the forward and reverse signals. Unwrapping performed by subtracting 2pi when we are outside the 180 deg range. Not 100% sure this works under all conditions.
-phase_difference = forward_phase - reverse_phase
+phase_difference = reverse_phase - forward_phase
 phase_difference[phase_difference > math.pi] = phase_difference[phase_difference > math.pi] - 2 * math.pi
 gamma_phase = np.mean(phase_difference)
 